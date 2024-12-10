8a33b8725c791d82c1c15207a8af8d2518268034
Joki <josedavidamayar@gmail.com>
2024-12-10 18:02:21 -0500

    feat: add mongoDB connection setup

---
+++ src/config/db.ts
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import "dotenv/config";
+
+const MONGO_URI = process.env.MONGO_URI || "";
+
+/**
+ * Establishes the connection with the db
+ */
+export const connectDb = async () => {
+  try {
+    await mongoose.connect(MONGO_URI);
+    console.log("Connection successful");
+  } catch (error) {
+    console.error("Error during the db connection: ", error);
+  }
+}
