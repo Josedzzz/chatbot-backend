2fa5a6ae63d04705b88b6af096c6839fb453a6fb
Joki <josedavidamayar@gmail.com>
2024-12-10 18:03:14 -0500

    feat: add mongoDB connection setup

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import express, { Application } from "express";
+import { connectDb } from "./config/db";
 
 const app: Application = express();
 
 // Middlewares
 app.use(express.json());
+
+// Connection with the db
+connectDb();
 
 // Port
 const PORT = process.env.PORT || 3000;
