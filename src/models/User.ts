0665a60a3a2eceb5c14b3d60127873a5f76eb85f
Joki <josedavidamayar@gmail.com>
2024-12-12 03:30:08 -0500

    feat: update User model to support single chat reference

--- src/models/User.ts
+++ src/models/User.ts
@@ -4,14 +4,14 @@ export interface IUser extends Document {
   username: string;
   email: string;
   password: string;
-  chats: mongoose.Types.ObjectId[];
+  chat: mongoose.Types.ObjectId;
 }
 
 const userSchema: Schema = new Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  chats: [{ type: Schema.Types.ObjectId, ref: "Chat" }],
+  chat: { type: Schema.Types.ObjectId, ref: "Chat" },
 });
 
 export default mongoose.model<IUser>("User", userSchema);
