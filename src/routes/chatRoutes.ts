cdbc5fdc832f0b3be214114f7de7f9c75c399eb1
Joki <josedavidamayar@gmail.com>
2024-12-12 03:30:59 -0500

    feat: add routes to chat, get and clear

--- src/routes/chatRoutes.ts
+++ src/routes/chatRoutes.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import { validateJwt } from "../middlewares/chatValidation";
-import { chatController } from "../controllers/chatController";
+import { chatController, chatHistoryController, clearHistoryController } from "../controllers/chatController";
 
 const router = express.Router();
 
 router.post("/message", validateJwt, chatController);
+router.post("/history/:userId", validateJwt, chatHistoryController);
+router.delete("/clearhistory/:userId", validateJwt, clearHistoryController);
 
 export default router;
